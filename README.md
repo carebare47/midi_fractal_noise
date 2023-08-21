224d69782a30a202d0a75a893bd7341a44e92598
carebare47 <tom@shadowrobot.com>
2023-08-21 21:05:52 +0100

    upadte readme, add argparse

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # midi_fractal_noise
+
+Tested on windows 11
+
+## Print usage and exit
+```bash
+C:\Python310\python.exe run.py -h
+```
+
+
+## List available midi devices
+```bash
+PS C:\Users\tom_q\Documents\Projects\midi_fractal_noise> C:\Python310\python.exe run.py -l
+Available midi devices:
+  0: Microsoft GS Wavetable Synth 0
+  1: Focusrite USB MIDI 1
+  2: AXE-FX II MIDI Out 2
+  3: MidiView 3
+
+Exiting...
+```
+
+
+## Run with chosen device
+```bash
+C:\Python310\python.exe run.py -m Focusrite
+```
--- run.py
+++ run.py
@@ -2,6 +2,21 @@ import time
 import rtmidi
 from fractal_noise import FractalNoiseGenerator
 from midi_coms import MidiOutWrapper
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--list', '-l', action='store_true', help='List available midi devices and exit')
+parser.add_argument('--midi_device', '-m', help='Overwrite default midi device, '\
+                                                'a substring match will do (run with -l to show available devices)')
+parser.add_argument('--hide_graph', '-g', action='store_true', help='Hide debug window')
+parser.add_argument('--print', '-p', action='store_true', help='Print value sent to midi device')
+
+args = parser.parse_args()
+list_midi_devices_only = args.list
+midi_port_search_term = 'Focusrite'
+if args.midi_device:
+    midi_port_search_term = args.midi_device
+print_value = args.print
 
 # minilogue_xd midi settings
 MINILOGUE_MIDI_VCF_CUTOFF_CC = 43
@@ -12,17 +27,22 @@ MINILOGUE_MIDI_CHANNEL = 2
 # fractal noise parameters
 OCTAVES = 5
 DT = 1.0/50.0
-SHOW_GRAPH = True
+SHOW_GRAPH = not args.hide_graph
 POINTS = 256
 SPAN = 1.0
 SPEED = 0.20
 
 midiout = rtmidi.MidiOut()
 # Swap for a string that partially matches your midi device
-midi_port_search_term = 'Focusrite'
 available_ports = midiout.get_ports()
-midi_port_number = None
+if list_midi_devices_only:
+    print("Available midi devices:")
+    for idx, port in enumerate(available_ports):
+        print(f"  {idx}: {port}")
+    print("\nExiting...\n")
+    exit(0)
 
+midi_port_number = None
 try:
     midi_port_number = [idx for idx, x in enumerate(available_ports) if midi_port_search_term in x][0]
 except IndexError as e:
@@ -49,7 +69,8 @@ with midiout:
         next_value = f.return_next_point()
         f.my_step()
         midi_wrapper.send_control_change(cc=MINILOGUE_MIDI_VCF_CUTOFF_CC, value=next_value, ch=MINILOGUE_MIDI_CHANNEL)
-        print(f"sent {next_value}")
+        if not print_value:
+            print(f"sent {next_value}")
         time.sleep(DT)
 
 del midiout
